b3c6cc01d6a5e8ec7c6815a4ba366f93df839171
tinykings <92499963+tinykings@users.noreply.github.com>
2024-03-30 09:18:00 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-# Check if running as root
-if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run as root."
-    exit 1
-fi
-
-# Fan control
-cp fan_control.yaml /etc/hhfc/
-cp hhfc-resume.service /etc/systemd/system/
-systemctl enable hhfc.service
-systemctl start hhfc.service
-systemctl enable hhfc-resume.service
-
 # Sunshine
-frzr-unlock
+sudo frzr-unlock
 wget https://github.com/LizardByte/Sunshine/releases/latest/download/sunshine.pkg.tar.zst
-pacman -U --noconfirm sunshine.pkg.tar.zst
+sudo pacman -U --noconfirm sunshine.pkg.tar.zst
+sudo setcap cap_sys_admin+p $(readlink -f $(which sunshine))
 cp sunshine.service ~/.config/systemd/user/
 systemctl --user enable sunshine
-setcap cap_sys_admin+p $(readlink -f $(which sunshine))
+sudo setcap cap_sys_admin+p $(readlink -f $(which sunshine))
 systemctl --user start sunshine
+
+# Fan control
+sudo cp fan_control.yaml /etc/hhfc/
+sudo cp hhfc-resume.service /etc/systemd/system/
+sudo systemctl enable hhfc.service
+sudo systemctl start hhfc.service
+sudo systemctl enable hhfc-resume.service
