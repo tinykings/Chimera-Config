200def910df01461ebc2fd347878fde5812a81f7
tinykings <92499963+tinykings@users.noreply.github.com>
2024-07-24 09:18:44 -0700

    Update setup.sh
    fix

--- setup.sh
+++ setup.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-# Sunshine
-sudo frzr-unlock
+## Sunshine ##
+# unlock os
+frzr-unlock
+# get install latest sunshine
 wget https://github.com/LizardByte/Sunshine/releases/latest/download/sunshine.pkg.tar.zst
-sudo pacman -U --noconfirm sunshine.pkg.tar.zst
-sudo setcap cap_sys_admin+p $(readlink -f $(which sunshine))
+pacman -U --noconfirm sunshine.pkg.tar.zst
+# setup services
 cp sunshine.service /home/gamer/.config/systemd/user/
 cp sunshine-restart.service /home/gamer/.config/systemd/user/
-systemctl --user enable sunshine
 cp suspend@.service /etc/systemd/system
-sudo systemctl daemon-reload && sudo systemctl enable suspend@$(whoami)
-cp suspend.target ~/.config/systemd/user
-systemctl --user daemon-reload
-systemctl --user enable sunshine-restart
-sudo setcap cap_sys_admin+p $(readlink -f $(which sunshine))
-systemctl --user start sunshine
+cp suspend.target /home/gamer/.config/systemd/user/
+systemctl daemon-reload && sudo systemctl enable suspend@$(whoami)
+machinectl shell gamer@ /bin/bash -c 'systemctl --user daemon-reload'
+machinectl shell gamer@ /bin/bash -c 'systemctl --user enable sunshine'
+machinectl shell gamer@ /bin/bash -c 'systemctl --user enable sunshine-restart'
+setcap cap_sys_admin+p $(readlink -f $(which sunshine))
+# start sunshine
+machinectl shell gamer@ /bin/bash -c 'systemctl --user start sunshine'
 
-# Fan control
-sudo cp fan_control.yaml /etc/hhfc/
-sudo cp hhfc-resume.service /etc/systemd/system/
-sudo systemctl enable hhfc.service
-sudo systemctl start hhfc.service
-sudo systemctl enable hhfc-resume.service
+## Fan control ##
+cp fan_control.yaml /etc/hhfc/
+cp hhfc-resume.service /etc/systemd/system/
+systemctl enable hhfc.service
+systemctl start hhfc.service
+systemctl enable hhfc-resume.service
